958f78d0f46702faad66af9cc8f9a70a74c9ce83
Azizbek <thedroxmc@gmail.com>
2025-03-21 05:22:50 +0500

    +100 aura

--- functions/userprofile.js
+++ functions/userprofile.js
@@ -9,11 +9,23 @@
 let profileIcon = document.querySelector(".profile-icon");
 let profileName = document.querySelector(".profile-name");
 let profileDate = document.querySelector(".profile-date");
+let profileStatus = document.querySelector(".profile-status");
 
 // ###########################################################
 // ##                                                       ##
 // ##                                                       ##
-// ##                     Miscellaneous                     ##
+// ##                    Main functionality                 ##
 // ##                                                       ##
 // ##                                                       ##
 // ###########################################################
+
+let profileNameContent = localStorage.getItem("candidateName");
+let profileIconContent = profileNameContent[0];
+let profileDateContent = localStorage.getItem
+("registrationDate");
+let profileStatusContent = localStorage.getItem("userStatus");
+
+profileIcon.innerHTML = profileIconContent;
+profileName.innerHTML = profileNameContent;
+profileDate.innerHTML += profileDateContent;
+profileStatus.innerHTML = profileStatusContent
